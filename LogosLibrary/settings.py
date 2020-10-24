f68f3673132cfa83de59e4833bdd1cd317bed080
matthewrdavis <70169874+matthewrdavis@users.noreply.github.com>
2020-10-23 22:42:53 -0500

    Update settings.py

--- LogosLibrary/settings.py
+++ LogosLibrary/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f^=3%^mt5x6q_p=2#!mtmoc--r!i=7n$^=c9undz=@dzmbw3)&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
